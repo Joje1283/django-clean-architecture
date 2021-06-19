916016074aa097b96a11edf4934359781137627d
Jaesik <joje1283@gmail.com>
2021-06-20 00:59:09 +0900

    Separate the config file.

--- config/settings/common.py
+++ config/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o3-7u(l6nzjj#f5o)pfooht14qomxr*3yu1$au*tjf#3f&+_#(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.30.1.40', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
---
+++ config/settings/dev.py
@@ -0,0 +1,3 @@
+from .common import *
+
+ALLOWED_HOSTS += ['localhost', '127.0.0.1', '192.168.35.91', ]
