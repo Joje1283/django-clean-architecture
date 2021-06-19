4278c21f2e8dceb06ed5eb085f9693523957d9ee
Jaesik <joje1283@gmail.com>
2021-06-19 13:45:01 +0900

    Fix ROOT_URLCONF in settings.py  - Fix error due to project name change

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'clean_architecture_django.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
