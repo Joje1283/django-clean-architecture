3742b3a8827ba5013e3f95709ce9b64d23961683
Jaesik <joje1283@gmail.com>
2021-06-20 01:12:22 +0900

    Fix the data structure of response in reports API.

--- reports/views.py
+++ reports/views.py
@@ -2,7 +2,6 @@ import json
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.forms import model_to_dict
 
 from .models import Report
 
@@ -12,4 +11,5 @@ def reports(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         result = Report.objects.create(data=json.dumps(data))
-        return JsonResponse({'message': 'POST - hello django server', 'data': model_to_dict(result)})
+        return JsonResponse(
+            {'message': 'POST - hello django server', 'result': {'id': result.pk, 'data': json.loads(result.data)}})
