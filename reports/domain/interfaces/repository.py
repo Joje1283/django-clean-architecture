2347c2de8616db5eb691ac3401200f6e4a391e39
Jaesik <joje1283@gmail.com>
2021-07-23 09:18:53 +0900

    Seperate ReportDataAccess files.

---
+++ reports/domain/interfaces/repository.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from ..entities import Report
+
+
+class ReportDataAccess(ABC):
+    @abstractmethod
+    def get_recently_reports(self) -> List[Report]:
+        pass
+
+    @abstractmethod
+    def create_report(self, report: Report) -> Report:
+        pass
