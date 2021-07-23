666fae13fc9900498b4834a3ccf1035eeb53f7d1
Jaesik <joje1283@gmail.com>
2021-07-23 19:15:06 +0900

    Add GetReportsInputBoundary interface for dependency injection.

--- reports/domain/usecases/get_recently_reports.py
+++ reports/domain/usecases/get_recently_reports.py
@@ -1,11 +1,21 @@
 from __future__ import annotations
 from typing import List
+from abc import ABC, abstractmethod
 
 from ..entities import Report
 from reports.domain.interfaces.repository import ReportDataAccess
 
 
-class GetRecentlyReportsInteractor:
+class GetReportsInputBoundary(ABC):
+    @abstractmethod
+    def set_params(self) -> GetReportsInputBoundary:
+        pass
+
+    def execute(self) -> List[Report]:
+        pass
+
+
+class GetRecentlyReportsInteractor(GetReportsInputBoundary):
     def __init__(self, report_repo: ReportDataAccess):
         self.report_repo: ReportDataAccess = report_repo
 
